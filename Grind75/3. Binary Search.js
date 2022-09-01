5ed646f2af38386f294a4ebfd074869750862cda
William Pasternak <wjp09@yahoo.com>
2022-09-01 14:48:43 -0400

    Last Challenge: Binary Search

--- Grind75/3. Binary Search.js
+++ Grind75/3. Binary Search.js
@@ -3,18 +3,19 @@ const search = function(nums, target) {
     let right = nums.length -1
   
 while(left <= right){
-    let middle = Math.floor(( left + right ) / 2)
+    let middle = Math.floor(( left + right ) / 2) // Gets middle value
     let guess = nums[middle]
     
-if(guess === target){
+if(guess === target){ // If middle number = target, return index of middle
 	return middle
 }  
-if(target > guess){
+if(target > guess){	 // If target is larger than middle, move leftmost bound
 	left = middle + 1
 }
-  else if(target < guess){
+  else if(target < guess){ // If target is smaller than middle, move rightmost bound
   	right = middle - 1
   }
 }
-return -1
+return -1	// If target is not found, return -1
 };
+
