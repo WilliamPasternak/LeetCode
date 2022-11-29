4f3a67ff26f4e328cfcddc7adb5056073d2e190f
William Pasternak <31456405+WilliamPasternak@users.noreply.github.com>
2022-11-29 17:46:34 -0500

    Add comments

--- LeetCode-Patterns/1. Contains Duplicates.js
+++ LeetCode-Patterns/1. Contains Duplicates.js
@@ -1,8 +1,35 @@
 // Given an integer array nums, return true if any value appears more than once in the array, and return false if every element is unique.
 
+// Brute Force
+// Time: O(N^2) Space: O(1)
+let containsDuplicate = function(nums) {
+    for(let i = 0; i < nums.length -1; i++){
+        for(let j = i + 1; j < nums.length; j++){
+            if(nums[i] === nums[j]){
+                return true
+            }
+        }     
+    }
+    return false
+};
+
+
+// Time: O(n) Space: O(n)
 function containsDuplicate(nums) {
    // use Set to store unique array element 
     let uniqueNums = new Set(nums);
 	// If the count of unique numbers is equal to the length of the original array, there are no duplicates.
     return uniqueNums.size !== nums.length;
 };
+
+
+
+// Time: O(n) Space: O(n)
+function containsDuplicate(nums){
+   let uniqueNums = new Set()
+   for(let num of nums){
+      if(uniqueNums.has(num)) return true
+      uniqueNums.add(num)
+   }   
+   return false
+}
