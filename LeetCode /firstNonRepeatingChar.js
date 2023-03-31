af6c1e1f93ff694d72ef6d75ca8df3fcbba5f12a
William Pasternak <31456405+WilliamPasternak@users.noreply.github.com>
2023-03-31 00:08:50 -0400

    “firstNonRepeatingChar”

--- LeetCode /firstNonRepeatingChar.js
+++ LeetCode /firstNonRepeatingChar.js
@@ -1,4 +1,11 @@
-// Time: O(N) Space: O(K)
+First Non Repeating Character
+Write a function that takes in a string of lowercase English-alphabet letters and returns the index ‹ the string's first non-repeating character.
+The first non-repeating character is the first character in a string that occurs only once.
+If the input string doesn't have any non-repeating characters, your function should return
+- 1
+
+// Time: O(N)  - 2 independent traversals.
+// Space: O(1) - lookUp's max size is 26 as only lowercase English letters are used.
 function firstNonRepeatingCharacter(string) {
   
   // Count character frequency in string
