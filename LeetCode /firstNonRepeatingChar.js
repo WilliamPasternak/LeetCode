ea11e96c40a3642db12d1675c5e34f8d7340ef58
William Pasternak <31456405+WilliamPasternak@users.noreply.github.com>
2023-04-02 00:10:02 -0400

    Last Challenge: First Non Repeating Character

--- LeetCode /firstNonRepeatingChar.js
+++ LeetCode /firstNonRepeatingChar.js
@@ -1,4 +1,5 @@
 First Non Repeating Character
+
 Write a function that takes in a string of lowercase English-alphabet letters and returns the index ‹ the string's first non-repeating character.
 The first non-repeating character is the first character in a string that occurs only once.
 If the input string doesn't have any non-repeating characters, your function should return
