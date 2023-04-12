4cbc6d202a625bd731cafd1eb43175f8ff81982e
William Pasternak <31456405+WilliamPasternak@users.noreply.github.com>
2023-04-11 22:07:33 -0400

    Last Challenge: Reverse Linked List

--- LeetCode /206. Reverse Linked List.js
+++ LeetCode /206. Reverse Linked List.js
@@ -13,3 +13,28 @@ function reverseList(head){
         head = next 
     }
 }
+
+
+function reverseList(head) {
+    // Initialize two variables to keep track of the previous node and the next node
+    let prev = null;
+    let next = null;
+
+    // While the "head" pointer is not null...
+    while (head) {
+        // Store a reference to the next node, since we're about to overwrite it
+        next = head.next;
+
+        // Point the current node's "next" pointer back at the previous node
+        head.next = prev;
+
+        // Move the "prev" pointer forward to the current node
+        prev = head;
+
+        // Move the "head" pointer forward to the next node in the original list
+        head = next;
+    }
+
+    // When we reach the end of the list (head is null), "prev" will be pointing to the new head of the reversed list, so return "prev"
+    return prev;
+}
