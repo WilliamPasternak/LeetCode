fd546cdaf298ecf48ff1872d5b4415d670cca577
William Pasternak <wjp09@yahoo.com>
2023-05-26 21:15:16 -0400

    Last Challenge: Roman Numeral to Integer

--- LeetCode /[13] Roman to Integer.js
+++ LeetCode /[13] Roman to Integer.js
@@ -1,14 +1,14 @@
 13. Roman to Integer
 Given a roman numeral as a string (s), convert it to an integer.
 
-var romanToInt = function(s) {
-    const map = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000};
-    let num = 0;
+function romanToInt(s) {
+    const values = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000};
+    let total = 0;
     
     for(let i = 0; i < s.length; i++) {
-        const curr = map[s[i]], next = map[s[i+1]];
-        if(curr < next) num -= curr;
-        else num += curr;
+        const [currentNum,nextNum] = [values[s[i]] ,values[s[i+1]]]
+        if(currentNum < nextNum) total -= currentNum;
+        else total += currentNum;
     }
-    return num;    
+    return total;    
 };
