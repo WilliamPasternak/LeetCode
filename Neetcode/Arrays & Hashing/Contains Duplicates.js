fc9c0564ca7ace93e4e69013414134d1900fef6c
William Pasternak <wjp09@yahoo.com>
2024-01-06 13:37:40 -0500

    Last Challenge: Contains Duplicates'

--- Neetcode/Arrays & Hashing/Contains Duplicates.js
+++ Neetcode/Arrays & Hashing/Contains Duplicates.js
@@ -1,50 +1,30 @@
-// Given an integer array nums, return true if any value appears more than once in the array, and return false if every element is unique.
+/**
+ * Challenge: Contains Duplicates
+ * Source: https://leetcode.com/problems/contains-duplicate/
+ * Description: Given an integer array nums, return true if any value appears at least twice in the array, 
+ * and return false if every element is distinct.
+ * /
 
-// Brute Force
-// Time: O(N^2) Space: O(1)
-let containsDuplicate = function(nums) {
-    for(let i = 0; i < nums.length -1; i++){
-        for(let j = i + 1; j < nums.length; j++){
-            if(nums[i] === nums[j]){
-                return true
-            }
-        }     
+/**
+ * Given an integer array nums, return true if any value appears at least twice in the array, 
+ * and return false if every element is distinct.    
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+function containsDuplicate(nums) {
+    // Create an object to hold numsSeen
+    const numsSeen = {}
+    // Iterate over nums array, add each number to numsSeen.
+    for(let i = 0; i < nums.length; i++){
+        const currentNumber = nums[i]
+        // If current number exists in numsSeen, duplicate found, return true.
+        if(numsSeen[currentNumber]) return true
+        numsSeen[currentNumber] = 1
     }
     return false
-};
-
-
-// Time: O(n) Space: O(n)
-function containsDuplicate(nums) {
-   // use Set to store unique array element 
-    let uniqueNums = new Set(nums);
-	// If the count of unique numbers is equal to the length of the original array, there are no duplicates.
-    return uniqueNums.size !== nums.length;
-};
-
-
-
-// Time: O(n) Space: O(n)
-function containsDuplicate(nums){
-   let uniqueNums = new Set()
-   for(let num of nums){
-      if(uniqueNums.has(num)) return true
-      uniqueNums.add(num)
-   }   
-   return false
 }
 
-// Time: O(n) Space: O(n)
-function containsDuplicate(nums) {
-   const lookup = {}
-   for(let num of nums){
-      if(lookup[num]) return true
-      lookup[num] = true
-  }
-  return false
-};
-
 // Tests:
-console.log(containsDuplicate([1,2,3,1]),'true')
-console.log(containsDuplicate([1,2,3,4]),'false')
-console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]),'true')
+console.log(containsDuplicate([1,2,3,1]),true)
+console.log(containsDuplicate([1,2,3,4]),false)
+console.log(containsDuplicate([11,1,1,2,2,3,3,3,2,4,2]),true)
