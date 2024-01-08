cb6d770de6e097402ebd49c226a2f08274d3e837
William Pasternak <wjp09@yahoo.com>
2024-01-08 13:21:49 -0500

    Add challenge instructions

--- Neetcode/Binary Search/Binary Search.js
+++ Neetcode/Binary Search/Binary Search.js
@@ -1,4 +1,11 @@
 /**
+ * Challenge: Binary Search
+ * Source: https://leetcode.com/problems/binary-search/
+ * Description: Given an array of integers nums which is sorted in ascending order, and an integer target, 
+ * write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+ * Note: You must write an algorithm with O(log n) runtime complexity.
+ * 
+/**
  * @param {number[]} nums
  * @param {number} target
  * @return {number} representing index of target number in nums.
