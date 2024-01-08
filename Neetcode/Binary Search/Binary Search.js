1d720c03f31c1ad6aa3235cb76f172f8fced84e5
William Pasternak <wjp09@yahoo.com>
2024-01-08 13:15:20 -0500

    Last Challenge: Binary Search

--- Neetcode/Binary Search/Binary Search.js
+++ Neetcode/Binary Search/Binary Search.js
@@ -1,16 +1,23 @@
-Binary Search
-Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
-
-You must write an algorithm with O(log n) runtime complexity.
-
-function search(arr, elem) {
-    let start = 0;
-    let end = arr.length - 1;
-    let middle = Math.floor((start + end) / 2);
-    while(arr[middle] !== elem && start <= end) { 
-        if(elem < arr[middle]) end = middle - 1; 
-        else start = middle + 1; 
-        middle = Math.floor((start + end) / 2);
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number} representing index of target number in nums.
+ */
+function search(nums, target) {
+    // establish left & right indicies.
+    let [leftIndex,rightIndex] = [0,nums.length -1]
+    while(leftIndex <= rightIndex){
+        // Establish middle index.
+        let middleIndex = Math.floor((leftIndex + rightIndex) / 2)
+        if(nums[middleIndex] === target) return middleIndex
+        else if(target < nums[middleIndex]) rightIndex = middleIndex - 1
+        else if(target > nums[middleIndex]) leftIndex = middleIndex  + 1
     }
-    return arr[middle] === elem ? middle : -1; 
-}
+    // if target was not found in nums.
+    return -1
+}   
+// Tests
+console.log(search([-1,0,3,5,9,12],9),4) // Target in nums at index 4.
+console.log(search([-1,0,3,5,9,12],2),-1)  //  Target not in nums.
+console.log(search([],5),-1)  // Empty array. Target not in nums.
+console.log(search([7], 7), 0)  // Single element array. Target in nums at index 0.
