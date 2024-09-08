15b29e6547b5475c0217d8a18b51dec3205080a2
William Pasternak <wjp09@yahoo.com>
2024-09-08 16:41:20 -0400

    Last Challenge: Is Subsequence

--- LeetCode /[392] Is Subsequence.js
+++ LeetCode /[392] Is Subsequence.js
@@ -1,24 +1,35 @@
+392. Is Subsequence
+
 Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
 
 A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
 
-/**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
- */
-function isSubsequence(s, t) {
-    let [sIdx, tIdx] = [0,0]
-    while (sIdx < s.length && tIdx < t.length) {
-        console.log(sIdx)
-        if (s[sIdx] === t[tIdx]) {
-            sIdx++
-        } 
-            tIdx++
-        }
-    return sIdx === s.length 
-}
+Example 1:
+Input: s = "abc", t = "ahbgdc"
+Output: true
 
-// Tests
-console.log(isSubsequence('abc','ahbgdc') , 'true')
-console.log(isSubsequence('axc','ahbgdc'), 'false')
+Example 2:
+Input: s = "axc", t = "ahbgdc"
+Output: false
+
+var isSubsequence = function(s, t) {
+    // create 2 pointers starting at 0
+    let [left,right] = [0,0]
+    // Iterate over str 't'
+    while (right <= t.length) {
+        if (left >= s.length) {
+            return true
+        }
+        // If character in left, is found in right, advance pointers
+        if (s[left] === t[right]){
+            left++
+            right++
+        }
+        // If character is not found, advance right pointer only.
+        else {
+            right++
+        }
+    }
+    // If right pointer advances past length of str 't' return false.
+    return false
+};
